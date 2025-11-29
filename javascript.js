fd1a12d02fd44b5b6d4119bcd2ae0be35d602e99
LinaDelocour <polinadorohykh@gmail.com>
2025-11-29 13:09:18 +0000

    Completed Java Script of the top part of the app

--- javascript.js
+++ javascript.js
@@ -1,18 +1,44 @@
-function title(event){
-  event.preventDefault();
-  let search=document.querySelector("#search-input");
-  let city=document.querySelector("#current-city");
-  city.innerHTML=search.value;
+function refresh(response){
+  cityElement=document.querySelector("#current-city");
+  cityElement.innerHTML=response.data.city;
+  
+  let temperatureElement=document.querySelector("#current-temperature-value");
+  let temperature=response.data.temperature.current;
+  temperatureElement.innerHTML=Math.round(temperature);
+
+  let condition=document.querySelector("#condition");
+  condition.innerHTML=response.data.condition.description;
+
+  let humidity=document.querySelector("#humidity");
+  humidity.innerHTML=response.data.temperature.humidity;
+
+  let wind=document.querySelector("#wind");
+  let speed=`${response.data.wind.speed}km/h`;
+  wind.innerHTML=speed;
+
+  let icon=document.querySelector("#icon");
+  let iconUrl=`<img src="${response.data.condition.icon_url}">`;
+  icon.innerHTML=iconUrl;
 }
 
-function date(dates) {
+function api(city){
+  let apiKey = "ae3a312323bto5f9e8049a27984bd0d3";
+  let apiURL=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiURL).then(refresh);
+}
+function title(event){
+  event.preventDefault();
+  let search = document.querySelector("#search-input");
+  api(search.value);
+}
+function date(dates){
   let minutes = dates.getMinutes();
   let hours = dates.getHours();
   let day = dates.getDay();
 
   if (hours < 10) {
     hours = `0${hours}`;
-  }
+  };
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
@@ -28,7 +54,7 @@ function date(dates) {
   ];
   let newDay = days[day];
   return `${newDay} ${hours}:${minutes}`;
-}
+};
 
 let now = new Date();
 let formatDate = document.querySelector("#current-date");
